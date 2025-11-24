7bed7f9438100318ad5f61c9f7f54c5d6b0be09f
Yaswitha Sakhamuri <yaswitha@example.com>
2025-11-24 22:27:33 +0530

    added jacoco report

--- src/main/java/com/flightapp/FlightWebfluxMongodbApplication.java
+++ src/main/java/com/flightapp/FlightWebfluxMongodbApplication.java
@@ -3,7 +3,10 @@ package com.flightapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+})
 public class FlightWebfluxMongodbApplication {
 
 	public static void main(String[] args) {
