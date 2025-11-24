d15621bba2aff75f48a144e4be5a13361b1c2a02
Yaswitha Sakhamuri <yaswitha@example.com>
2025-11-24 12:24:23 +0530

    updated controller tests

--- src/test/java/com/flightapp/controller/AirlineControllerTest.java
+++ src/test/java/com/flightapp/controller/AirlineControllerTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.flightapp.entity.Airline;
--- src/test/java/com/flightapp/controller/PassengerControllerTest.java
+++ src/test/java/com/flightapp/controller/PassengerControllerTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -14,6 +15,7 @@ import com.flightapp.service.PassengerService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@WebFluxTest(PassengerController.class)
 public class PassengerControllerTest {
 	@Autowired
     private WebTestClient webClient;
